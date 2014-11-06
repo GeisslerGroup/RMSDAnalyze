Building new running average code, still have bug.

RMSDAnalyze/grid.py
@@ -51,6 +51,11 @@ class PlotLabeler:
         self.colorrange=colorrange
         self.colormap=colormap
 
+def GetMtxScale(extent, gridsize):
+    L = (extent[1] - extent[0], extent[3] - extent[2])
+    mtx_scale = np.array( [2.*float(gridsize[0]) / L[0],    
+                           2.*float(gridsize[1]) / L[1]] )
+    return mtx_scale
 
 class SlabCoords:
     def __init__(self, dir1_extent, dir2_extent, dir1=0, dir2=2, thickness=1):
@@ -68,21 +73,26 @@ class SlabCoords:
         # dir_trunc computes the remaining direction
         self.dir_trunc= sum([0,1,2]) - dir1 - dir2 
         self.thickness = thickness
-    def GetMtxScale(self, gridsize):
-        return max(gridsize[0] / self.dir1_extent, gridsize[1] / (2 * self.dir2_extent)) * 100
-    def ProcessSparseRunner(self, running_mean, running_weight, mtx_scale):
+    def ProcessSparseRunner(self, running_mean, running_weight, gridsize):
         extent = self.GetExtent()
-        running_mean   = mtx.csr_matrix(running_mean/running_weight)
-        running_mean   = running_mean.tocoo()
-        running_weight = running_weight.tocoo()
+        mtx_scale = GetMtxScale(self.GetExtent(), gridsize)
+        nonzero = (running_weight != 0)
+        running_mean   = running_mean[nonzero] / running_weight[nonzero]
+        running_weight = running_weight[nonzero]
+
+        #TODO: Fix this reversal process
+        #TODO: Force the UpdateSparseRunner to use the GetMtxScale from the coords
+        #TODO: Move this function outside of the classes!
         #print "RUNNING MEAN TYPE: {}".format(type(running_mean_mtx))
         #print "RUNNING MEAN DATA: {}".format(running_mean_mtx)
-        data_pos  = np.vstack((running_mean.row,running_mean.col)).astype(float).T / mtx_scale
-        data_pos += np.array([extent[0],extent[2]])
-        weight_pos = np.vstack((running_weight.row,running_weight.col)).astype(float).T / mtx_scale
-        weight_pos += np.array([extent[0],extent[2]])
-        weight_mean = running_weight.data
-        return (running_mean.data, data_pos), (weight_mean, weight_pos)
+        logging.debug("Matrix scale: {}, shape = {}".format(mtx_scale, mtx_scale.shape))
+        x = np.arange(0, 2*gridsize[0] + 1, dtype=float) / mtx_scale[0]
+        y = np.arange(0, 2*gridsize[1] + 1, dtype=float) / mtx_scale[1]
+        xv, yv = np.meshgrid(x,y)
+        data_x = xv[nonzero]
+        data_y = yv[nonzero]
+        data_pos = np.hstack(data_x, data_y)
+        return (running_mean, data_pos), (running_weight, data_pos)
     def GetExtent(self):
         return [-self.dir1_extent, self.dir1_extent, -self.dir2_extent, self.dir2_extent]
     def __call__(self, r_ik, op_i=None):
@@ -133,15 +143,23 @@ class RadialCoords:
 
 
 def ComputeRMSD(r0_ik, r1_ik, pre_cutoff=None, post_cutoff=None, rmsd_lambda = None):
+    """
+    Compute the quantities that, when averaged, allow for a computation of RMSD.
+    Returns OP_i = [|r|^2, dx, dy, dz]
+    """
     dr_ik = r0_ik- r1_ik 
-    rmsd_i = np.sqrt(np.sum(np.square(dr_ik), axis=1))
-    if pre_cutoff:
-        rmsd_i[rmsd_i > pre_cutoff] = None
-    if rmsd_lambda:
-        rmsd_i = rmsd_lambda(rmsd_i)
-    if post_cutoff:
-        rmsd_i[rmsd_i > post_cutoff] = None
-    return rmsd_i
+    magsq_dr_i = np.sum(np.square(dr_ik), axis=1)
+    #if pre_cutoff:
+    #    rmsd_i[rmsd_i > pre_cutoff] = None
+    #if rmsd_lambda:
+    #    rmsd_i = rmsd_lambda(rmsd_i)
+    #if post_cutoff:
+    #    rmsd_i[rmsd_i > post_cutoff] = None
+    magsq_dr_i.shape = (len(magsq_dr_i), 1)
+    op_i = np.hstack([magsq_dr_i, dr_ik])
+    return op_i
+
+
 
 def ComputeRMSAngle(r0_ik, r1_ik, rmsd_lambda = None):
     r0_v = (r0_ik[0::3] - r0_ik[1::3]) + \
@@ -185,36 +203,55 @@ def OPPlotter2D(x,y, value, extent, gridsize, plotlabeler=PlotLabeler, style='he
     plt.axis('equal')
     plt.tight_layout()
 
-def UpdateRunningMean2D(running_mean_mtx, running_weight_mtx, x, y, value, extent, gridsize, mtx_scale=100, style='hex'):
+def UpdateRunningMean2D(running_mean_mtx, running_weight_mtx, x, y, value, extent, gridsize, style='hex'):
     logging.debug("Number of entries: {}".format(x.shape))
+    value_shape  = (2*gridsize[0]+1, 2*gridsize[1]+1, value.shape[1])
+    weight_shape = (value_shape[0], value_shape[1], 1)
+    if running_mean_mtx == None:
+        running_mean_mtx = np.zeros(value_shape)
+    if running_weight_mtx == None:
+        running_weight_mtx = np.zeros(weight_shape)
     if style=='hex':
-    # Compute the mean RMSD per box and the weight of the box
-        meanplt   = plt.hexbin(x, y, C=value, \
-                mincnt=0, gridsize = gridsize, extent=extent) 
-        weightplt = plt.hexbin(x, y, \
-                mincnt=0, gridsize = gridsize, extent=extent) 
+        mean_mtx   = np.zeros(value_shape)
+        weight_mtx = np.zeros(weight_shape)
+        mtx_scale = GetMtxScale(extent, gridsize)
+        
+        logging.debug("Shape of mean_mtx: {}".format(mean_mtx.shape))
+        # Compute the mean of all of the values in "value" 
+        for k in xrange(value.shape[1]):
+            meanplt   = plt.hexbin(x, y, C=value[:,k], mincnt=0, 
+                    gridsize = gridsize, extent=extent) 
+            plt.clf()
+            mean   = meanplt.get_array()
+            mean_pos   = mtx_scale * (meanplt.get_offsets() 
+                    - np.array([extent[0],extent[2]]))
+            mean_pos = np.abs(np.round(mean_pos))
+            mean_pos = mean_pos.astype(int)
+            logging.debug("Mean pos values: {}".format(mean_pos))
+
+            logging.debug("Shape of mean-value data: {}".format(mean.shape))
+            logging.debug("Extent of mean-value data: {}".format( 
+                    [min(mean_pos[:,0]), max(mean_pos[:,0]), 
+                     min(mean_pos[:,1]), max(mean_pos[:,1])] ))
+
+            mean_mtx  [mean_pos[:,0], mean_pos[:,1],k] = mean[:]
+        # Compute the statistical weight of those entries
+        weightplt = plt.hexbin(x, y, mincnt=0, 
+                gridsize = gridsize, extent=extent) 
+        plt.clf()
+        weight = weightplt.get_array()
+        weight_pos = mtx_scale * (weightplt.get_offsets() 
+                - np.array([extent[0],extent[2]]))
+        weight_pos = np.round(weight_pos)
+        weight_pos = weight_pos.astype(int)
+        logging.debug("Shape of weight data: {}".format(weight.shape))
+        weight_mtx[weight_pos[:,0], weight_pos[:,1], 0] = weight[:]
+
+        running_mean_mtx   += mean_mtx * weight_mtx
+        running_weight_mtx += weight_mtx
+        return running_mean_mtx, running_weight_mtx
     else:
         raise ValueError("Plotting style must be 'hex', received {}". format(style))
-    plt.clf()
-    #plt.show()
-    mean   = meanplt.get_array()
-    weight = weightplt.get_array()
-    #print "mean shape, weight shape: {}, {}".format(hex_mean.shape, hex_weight.shape)
-    mean_pos   =  mtx_scale * (meanplt.get_offsets()   - np.array([extent[0],extent[2]]))
-    weight_pos =  mtx_scale * (weightplt.get_offsets() - np.array([extent[0],extent[2]]))
-    mean_pos.astype(int)
-    weight_pos.astype(int)
-    logging.debug("Shape of mean-value data: {}".format(mean.shape))
-    logging.debug("Shape of weight data: {}".format(weight.shape))
-    mean_mtx   = mtx.csr_matrix( (mean  , (mean_pos[:,0],   mean_pos[:,1]  )), dtype='f')
-    weight_mtx = mtx.csr_matrix( (weight, (weight_pos[:,0], weight_pos[:,1])) )
-    if running_mean_mtx == None and running_weight_mtx == None:
-        running_mean_mtx   = mean_mtx.multiply(weight_mtx)
-        running_weight_mtx = weight_mtx
-    else:
-        running_mean_mtx   = running_mean_mtx + mean_mtx.multiply(weight_mtx)
-        running_weight_mtx = running_weight_mtx + weight_mtx
-    return running_mean_mtx, running_weight_mtx
 
 
 
@@ -257,7 +294,7 @@ def GridOP(data_tik, display_type=[], dynamic_step = 0, colorrange=[None,None], 
     atom_type = 'water'
     running_mean_mtx = None
     running_weight_mtx = None
-    mtx_scale = coord_system.GetMtxScale(gridsize)
+    mtx_scale = GetMtxScale(coord_system.GetExtent(), gridsize)
     for t0 in xrange(data_tik.shape[0] - dynamic_step):
         #print "outputting time {} of {}".format(t0, data_tik.shape[0])
         atoms = [data_tik[t0,:,:]]
@@ -270,7 +307,7 @@ def GridOP(data_tik, display_type=[], dynamic_step = 0, colorrange=[None,None], 
         extent = coord_system.GetExtent()
         r,z,op_i = coord_system(r_ik, op_i)
         running_mean_mtx, running_weight_mtx = UpdateRunningMean2D(running_mean_mtx, running_weight_mtx, r, z, op_i, \
-                                                                    extent, gridsize, mtx_scale=mtx_scale)
+                                                                    extent, gridsize)
     (data, data_pos), (density,density_pos) = coord_system.ProcessSparseRunner(running_mean_mtx, running_weight_mtx, mtx_scale)
 
     # PLOTTING FUNCTION -- should be separate function, but it shares too many arguments